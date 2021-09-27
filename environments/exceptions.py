cb6191b67760cead432a65313a2977d7fa6d1210
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-09-27 16:31:36 +0300

    Change message of InvalidEnvironmentSetup

--- environments/exceptions.py
+++ environments/exceptions.py
@@ -4,13 +4,8 @@ class InvalidEnvironmentSetup(Exception):
 
     def __init__(self, message=None, env=None):
         if not message:
-            message = "Invalid value of env parameter. [QA] or [Production] values are allowed"
+            message = "Invalid value of env parameter. [QA] or [Production] values are allowed. " \
+                      "Please use environments.enums.Enviroments enum for configure environment properly"
+
         self.message = message
         self.env = env
-
-
-class BadAuthResponse(Exception):
-    def __init__(self, message=None):
-        if not message:
-            message = "Bad Response. Response could is not verified."
-        self.message = message

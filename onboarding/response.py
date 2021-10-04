01be9b93014751c115da284ce58c34d076374c08
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-04 13:30:43 +0300

    Implement get_connection_criteria, get_sensor_alternate_id, get_authentication methods for BaseOnboardingResonse

--- onboarding/response.py
+++ onboarding/response.py
@@ -6,6 +6,18 @@ class BaseOnboardingResonse:
     def __init__(self, http_response: Response):
         self.response:  Response = http_response
 
+    def get_connection_criteria(self) -> dict:
+        response_data = self.data()
+        return response_data.get("connectionCriteria")
+
+    def get_sensor_alternate_id(self):
+        response_data = self.data()
+        return response_data.get("sensorAlternateId")
+
+    def get_authentication(self):
+        response_data = self.data()
+        return response_data.get("authentication")
+
     @property
     def data(self):
         return self.response.json()

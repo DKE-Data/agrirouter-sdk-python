6a9bd3e1fe25f89114719307d5e551b69509cce6
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-05 16:12:01 +0300

    Implement MQttMessagingService. Refactor HttpMessagingService

--- messaging/services.py
+++ messaging/services.py
@@ -3,12 +3,13 @@ from abc import ABC, abstractmethod
 import requests
 
 from messaging.certification import create_certificate_file
+from messaging.clients.mqtt import MqttClient
 from messaging.messages import Message
 from messaging.request import MessageRequest
 from messaging.result import MessagingResult
 
 
-class AbstractClient(ABC):
+class AbstractMessagingClient(ABC):
 
     def create_message_request(self, parameters) -> MessageRequest:
         messages = []
@@ -27,7 +28,10 @@ class AbstractClient(ABC):
         ...
 
 
-class HttpMessagingClient(AbstractClient):
+class HttpMessagingService(AbstractMessagingClient):
+
+    def __init__(self, onboarding_response):
+        self.onboarding_response = onboarding_response
 
     def send(self, parameters) -> MessagingResult:
         request = self.create_message_request(parameters)
@@ -42,10 +46,41 @@ class HttpMessagingClient(AbstractClient):
         result = MessagingResult([parameters.get_message_id()])
         return result
 
+    def subscribe(self):
+        pass
 
-class MqttMessagingClient(AbstractClient):
+    def unsubscribe(self):
+        pass
 
-    def send(self, parameters) -> MessagingResult:
+
+class MqttMessagingService(AbstractMessagingClient):
+
+    def __init__(self,
+                 onboarding_response,
+                 on_message_callback: callable = None,
+                 ):
+
+        self.onboarding_response = onboarding_response
+        self.client = MqttClient(
+            client_id=self.onboarding_response.get_client_id(),
+            on_message_callback=on_message_callback,
+        )
+        self.client.connect(
+            self.onboarding_response.get_connection_criteria()["host"],
+            self.onboarding_response.get_connection_criteria()["port"]
+        )
+
+    def send(self, parameters, qos: int = 0) -> MessagingResult:
         mqtt_payload = self.create_message_request(parameters)
+        self.client.publish(
+            self.onboarding_response.get_connection_criteria()["measures"], mqtt_payload,
+            qos=qos
+        )
         result = MessagingResult([parameters.get_message_id()])
         return result
+
+    def subscribe(self):
+        pass
+
+    def unsubscribe(self):
+        pass

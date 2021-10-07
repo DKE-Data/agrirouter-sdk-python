0f00ca2c8eb82cb6ca34e214062c57183affa75d
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-07 14:09:37 +0300

    Implement CloudOnboardService, CloudOffboardService

---
+++ messaging/services/cloud.py
@@ -0,0 +1,71 @@
+from generated.cloud_provider_integration.cloud_virtualized_app_registration_pb2 import OnboardingRequest, \
+    OffboardingRequest
+from generated.messaging.request.request_pb2 import RequestEnvelope
+from messaging.encode import encode_message
+from messaging.enums import TechnicalMessageType
+from messaging.messages import EncodedMessage
+from messaging.parameters.service import MessageHeaderParameters, MessagePayloadParameters, CloudOnboardParameters, \
+    CloudOffboardParameters
+from messaging.services.messaging import AbstractService
+from utils.type_url import TypeUrl
+from utils.uuid_util import new_uuid
+
+
+class CloudOnboardService(AbstractService):
+
+    @staticmethod
+    def encode(parameters: CloudOnboardParameters) -> EncodedMessage:
+        message_header_parameters = MessageHeaderParameters(
+            application_message_id=parameters.get_application_message_id(),
+            application_message_seq_no=parameters.get_application_message_seq_no(),
+            team_set_context_id=parameters.get_team_set_context_id(),
+            mode=RequestEnvelope.Mode.Value("DIRECT"),
+            technical_message_type=TechnicalMessageType.CLOUD_ONBOARD_ENDPOINTS.value
+        )
+
+        onboarding_request = OnboardingRequest(
+            onboarding_requests=parameters.get_onboarding_requests()
+        )
+
+        message_payload_parameters = MessagePayloadParameters(
+            type_url=TypeUrl.get_type_url(OnboardingRequest.__name__),
+            value=onboarding_request.SerializeToString()
+        )
+
+        message_content = encode_message(message_header_parameters, message_payload_parameters)
+        encoded_message = EncodedMessage(
+            id_=new_uuid(),
+            content=message_content
+        )
+
+        return encoded_message
+
+
+class CloudOffboardService(AbstractService):
+
+    @staticmethod
+    def encode(parameters: CloudOffboardParameters) -> EncodedMessage:
+        message_header_parameters = MessageHeaderParameters(
+            application_message_id=parameters.get_application_message_id(),
+            application_message_seq_no=parameters.get_application_message_seq_no(),
+            team_set_context_id=parameters.get_team_set_context_id(),
+            mode=RequestEnvelope.Mode.Value("DIRECT"),
+            technical_message_type=TechnicalMessageType.CLOUD_OFFBOARD_ENDPOINTS.value
+        )
+
+        offboarding_request = OffboardingRequest(
+            endpoints=parameters.get_endpoints()
+        )
+
+        message_payload_parameters = MessagePayloadParameters(
+            type_url=TypeUrl.get_type_url(OffboardingRequest.__name__),
+            value=offboarding_request.SerializeToString()
+        )
+
+        message_content = encode_message(message_header_parameters, message_payload_parameters)
+        encoded_message = EncodedMessage(
+            id_=new_uuid(),
+            content=message_content
+        )
+
+        return encoded_message

67d19b076b531c3bf888b0cdc8767d6eb0d83eb3
Alexey <petrovsky.lexey@gmail.com>
2021-10-10 19:25:13 +0300

    Add linter and formatter

--- tests/onboarding_test/test_headers.py
+++ tests/onboarding_test/test_headers.py
@@ -6,11 +6,18 @@ from agrirouter.onboarding.headers import SoftwareOnboardingHeader
 class TestSoftwareOnboardingHeader:
     reg_code = "1AC2cs21W"
     test_object = SoftwareOnboardingHeader(reg_code=reg_code, application_id=1)
-    test_object_1 = SoftwareOnboardingHeader(reg_code=reg_code, application_id=1, content_type="json")
+    test_object_1 = SoftwareOnboardingHeader(
+        reg_code=reg_code, application_id=1, content_type="json"
+    )
 
     def test_get_header(self):
         assert isinstance(self.test_object.get_header(), dict)
-        assert self.test_object.get_header()["Authorization"] == "Bearer " + self.reg_code
-        assert self.test_object.get_header()["Content-Type"] == ContentTypes.APPLICATION_JSON.value
+        assert (
+            self.test_object.get_header()["Authorization"] == "Bearer " + self.reg_code
+        )
+        assert (
+            self.test_object.get_header()["Content-Type"]
+            == ContentTypes.APPLICATION_JSON.value
+        )
 
         assert self.test_object_1.get_header()["Content-Type"] == "json"

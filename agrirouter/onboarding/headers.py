61c63761057938f5c2649c44d87030d893d3968e
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-11 20:28:55 +0300

    Fix SoftwareOnboardingHeader

--- agrirouter/onboarding/headers.py
+++ agrirouter/onboarding/headers.py
@@ -35,7 +35,7 @@ class SoftwareOnboardingHeader(BaseOnboardingHeader):
         return self.params
 
     def sign(self, signature):
-        self.params["X-Agrirouter-Signature"] = signature.decode()
+        self.params["X-Agrirouter-Signature"] = str(signature)
 
     def _set_params(self, reg_code: str, application_id: str, signature: str, content_type: str):
         header = dict()

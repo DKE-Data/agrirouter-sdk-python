fdf133a759b532af1945fd4a80d79f4cb636a584
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-13 14:01:15 +0300

    Fix onboarding headers signing

--- agrirouter/onboarding/headers.py
+++ agrirouter/onboarding/headers.py
@@ -1,3 +1,4 @@
+import base64
 from abc import ABC, abstractmethod
 
 from agrirouter.constants.media_types import ContentTypes
@@ -34,16 +35,17 @@ class SoftwareOnboardingHeader(BaseOnboardingHeader):
     def get_header(self) -> dict:
         return self.params
 
-    def sign(self, signature):
-        self.params["X-Agrirouter-Signature"] = str(signature)
+    def sign(self, signature: bytes):
+        print(signature)
+        self.params["X-Agrirouter-Signature"] = base64.b64encode(signature).decode()
+        print(self.params["X-Agrirouter-Signature"])
 
     def _set_params(self, reg_code: str, application_id: str, signature: str, content_type: str):
         header = dict()
         header["Authorization"] = f"Bearer {reg_code}"
         header["Content-Type"] = content_type
         header["X-Agrirouter-ApplicationId"] = application_id
-        if signature:
-            header["X-Agrirouter-Signature"] = signature
+        header["X-Agrirouter-Signature"] = signature if signature else ""
 
         self.params = header
 

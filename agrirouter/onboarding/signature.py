1cfb30e43ad86bbdc729e10fd447ac0498614a14
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-13 19:45:02 +0300

    Fix create_signature method

--- agrirouter/onboarding/signature.py
+++ agrirouter/onboarding/signature.py
@@ -2,20 +2,24 @@ from cryptography.hazmat.primitives.serialization import load_pem_public_key, lo
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
-from pprint import pprint
 
 SIGNATURE_ALGORITHM = "SHA256withRSA"
 
 
-def create_signature(request_body: str, private_key: str) -> bytes:
+def to_hex(sign: bytes):
+    return sign.hex()
+
+
+def create_signature(request_body: str, private_key: str) -> str:
     private_key_bytes = bytearray(private_key.encode('utf-8'))
     private_key_data = load_pem_private_key(private_key_bytes, None)
     signature = private_key_data.sign(
-        request_body.encode('utf-8'),
+        request_body.encode("utf-8"),
         padding.PKCS1v15(),
         hashes.SHA256()
     )
-    return signature
+
+    return to_hex(signature)
 
 
 def verify_signature(request_body: str, signature: bytes, public_key: str) -> None:

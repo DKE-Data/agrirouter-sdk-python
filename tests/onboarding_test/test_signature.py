0f654bb349fe0ac2fe489f616dfae5f5d35caea6
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-15 18:09:24 +0300

    change with black

--- tests/onboarding_test/test_signature.py
+++ tests/onboarding_test/test_signature.py
@@ -7,6 +7,6 @@ from tests.constants import private_key, wrong_private_key
 
 
 def test_create_signature():
-    assert re.search(r"[\w]", create_signature('127.0.0.1', private_key))
+    assert re.search(r"[\w]", create_signature("127.0.0.1", private_key))
     with pytest.raises(ValueError):
-        assert re.search(r"[\w]", create_signature('127.0.0.1', wrong_private_key))
+        assert re.search(r"[\w]", create_signature("127.0.0.1", wrong_private_key))

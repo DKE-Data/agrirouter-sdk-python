ceeeac55395c8123ded677ca8b5f384293291d73
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-15 15:20:20 +0300

    add test_signature.py

---
+++ tests/onboarding_test/test_signature.py
@@ -0,0 +1,12 @@
+"""Test agrirouter/onboarding/signature.py"""
+
+import pytest
+import re
+from agrirouter.onboarding.signature import create_signature
+from tests.constants import private_key, wrong_private_key
+
+
+def test_create_signature():
+    assert re.search(r"[\w]", create_signature('127.0.0.1', private_key))
+    with pytest.raises(ValueError):
+        assert re.search(r"[\w]", create_signature('127.0.0.1', wrong_private_key))

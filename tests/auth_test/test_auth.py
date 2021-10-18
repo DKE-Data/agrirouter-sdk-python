349055bb5498ca5bade8464690139110d24019e5
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-18 15:40:10 +0300

    add new asserts

--- tests/auth_test/test_auth.py
+++ tests/auth_test/test_auth.py
@@ -9,17 +9,26 @@ from tests.constants import (
     ENV,
     application_id,
 )
+from re import search
 
 
 class TestAuthorization:
     def test_extract_auth_response(self):
         auth_client = Authorization(ENV, public_key=public_key, private_key=private_key)
         assert isinstance(auth_client.extract_auth_response(auth_result_url), object)
+        assert search(
+            "<agrirouter.auth.response.AuthResponse",
+            str(auth_client.extract_auth_response(auth_result_url)),
+        )
 
         auth_client = Authorization(
             "Production", public_key=public_key, private_key=private_key
         )
         assert isinstance(auth_client.extract_auth_response(auth_result_url), object)
+        assert search(
+            "<agrirouter.auth.response.AuthResponse",
+            str(auth_client.extract_auth_response(auth_result_url)),
+        )
 
     def test_get_auth_request_url(self):
         auth_params = AuthUrlParameter(
@@ -28,7 +37,9 @@ class TestAuthorization:
         auth_client = Authorization(
             "QA", public_key=public_key, private_key=private_key
         )
+        check_url = "https://agrirouter-qa.cfapps.eu10.hana.ondemand.com/application/"
         assert isinstance(auth_client.get_auth_request_url(auth_params), str)
+        assert search(check_url, auth_client.get_auth_request_url(auth_params))
 
     def test_get_auth_result(self):
         auth_client = Authorization(
@@ -37,3 +48,4 @@ class TestAuthorization:
         auth_response = auth_client.extract_auth_response(auth_result_url)
         auth_client.verify_auth_response(auth_response)
         assert isinstance(auth_response.get_auth_result(), dict)
+        assert auth_response.get_auth_result()["credentials"]

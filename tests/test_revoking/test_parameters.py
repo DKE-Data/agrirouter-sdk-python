738db91ab440f940cd7fda79a8f658494e14f5c9
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-10-27 18:47:53 +0300

    Fix revoking test

--- tests/test_revoking/test_parameters.py
+++ tests/test_revoking/test_parameters.py
@@ -8,15 +8,15 @@ class TestRevokingParameter:
     content_type = "json"
     account_id = "111"
     endpoint_ids = "endpoint_1"
-    utc_timestamp = "+03:00"
-    timestamp = "01-01-2021"
+    time_zone = "+03:00"
+    utc_timestamp = "01-01-2021"
     test_object = RevokingParameter(
         application_id=application_id,
         content_type=content_type,
         account_id=account_id,
         endpoint_ids=endpoint_ids,
         utc_timestamp=utc_timestamp,
-        timestamp=timestamp,
+        time_zone=time_zone
     )
 
     def test_get_header_params(self):
@@ -27,4 +27,4 @@ class TestRevokingParameter:
         assert self.test_object.get_body_params()["account_id"] == self.account_id
         assert self.test_object.get_body_params()["endpoint_ids"] == self.endpoint_ids
         assert self.test_object.get_body_params()["utc_timestamp"] == self.utc_timestamp
-        assert self.test_object.get_body_params()["timestamp"] == self.timestamp
+        assert self.test_object.get_body_params()["time_zone"] == self.time_zone

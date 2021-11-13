8e1d24e9ff2abe34cab1bf65e5e9b9bbbd016176
Alexey Petrovsky <petrovsky.lexey@gmail.com>
2021-11-13 23:15:33 +0300

    Update examp,es script

--- example_script.py
+++ example_script.py
@@ -1,4 +1,8 @@
+from pprint import pprint
+
 from agrirouter.generated.messaging.request.payload.account.endpoints_pb2 import ListEndpointsQuery
+from agrirouter.onboarding.response import SoftwareOnboardingResponse
+import time
 
 public_key = """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzGt41/+kSOTlO1sJvLIN
@@ -40,6 +44,28 @@ HjmgzBRxXFy5uph6Ue6dxyszaA==
 -----END PRIVATE KEY-----"""
 
 
+onboarding_response_mqtt = {
+    "deviceAlternateId": "2145df0e-3451-46cb-bf23-23191af66fce",
+    "capabilityAlternateId": "523e4623-68d2-43d4-a0cc-e2ada2f68b5e",
+    "sensorAlternateId": "1489638c-7bed-4205-ad77-8d11efdc779f",
+    "connectionCriteria": {
+        "gatewayId": "2",
+        "host": "dke-qa.eu10.cp.iot.sap",
+        "port": 8883,
+        "clientId": "2145df0e-3451-46cb-bf23-23191af66fce",
+        "measures": "measures/2145df0e-3451-46cb-bf23-23191af66fce",
+        "commands": "commands/2145df0e-3451-46cb-bf23-23191af66fce"
+    },
+    "authentication": {
+        "type": "PEM",
+        "secret": "JNKdNg8R0lwmFgvrUfOCc7inebr0h?!7Z9wL",
+        "certificate": "-----BEGIN ENCRYPTED PRIVATE KEY-----\nMIIE6zAdBgoqhkiG9w0BDAEDMA8ECMkL85F+LbPbAgMCAAAEggTI1CmRlnDUStBv\nTycvaRVFMCk1OuynhiOYRF6HBFFXBCxWKZa3WqTShLdf9iCel/NgtdZIiQsoD1LL\nMxVyh8pWAfLQ+pDJLvM6suQjHALt8dW5iTeCZ7R1gzFvPJ+xnDGFFytN7HmGSvHM\nQbcCOuEeIu8U6ENa6/+WmUwK9/ZMkLNqDHVKEGpI+lSJs8JWEE+S3Klmsxuq0dvz\nh6o3V7RKFwMfUZOQLHezGBDjLfEBdP+d2G87CY+LSzinL8pFhLwyrXFKfYWYoT0m\n5PkDdjfiVq3SJIUoQWnGrjaVVw4TV3WSxmhQnWbDwOQydr8DAiBxDMYoeK3rePpC\nwh6KATnBrovq1icqjonYDE0T+3Rs2SUbG+3+m9Zj4j46L2Sh9bUB6qxdw74Ck2/z\nAzJ1N+tB+RL7UvOpMOhmndMBl5qpx9dFFy8Z/N7w4YTQLZLN7chD8ApeFhCgvppt\nAGh8/VeWO54OC9ZOSHpxEl7sJz97jaHYNbw/lGbDk7cOZezwpA0NCWZ/Bb1vRDzy\n8EDX9s1hOA3jiy2T1RSyk2Rj/12pWdKtdSO8lMhMKC0B32Zr1F8rBJKDVzqFWuTt\nn+pXOKedyOA/ggyvYJdsltP8O4XB2oBN3WBdFK7Y1FG/tN30LsaqcnFTxab5v1Pp\ngq2dHu6Xy0TCMAw/DH3RmGXlGnDDWu86Zad7TjjrEZvpSIv4TTSCqqTvc4IN0xFX\nbKZCrY6JSkJWWnDMKrsRYOijUDvpAbYwZuTV9PAljYbt5YX778qxV9O0fNBQdaww\nNlfxU93jgr4g3E9nIzRxLu9S98hPbxKUnVYiQmYvP7vJUcUSo5F0LmUU/nvHY1pi\nr4tZDp8Xu1aZy7cOd3sTbf/68IjiZMZlF5/PVlOFOo40yGqW600j/qEqXoY/492h\nONXUCpHKaG/Pkjtg9THuYoaw1773gxYYsYLt+c6NkQCCsydOr2BMZQ4Qy4bZV67D\n2RNDeZzSBY6jEX6dnfY0FJqIsSiw28Ek5NXx0HTEGN8txPkx/1dfu3RfZnzUqT/0\nmS9xcWVYRmlip3vm48fMecqP/DNIHyjVLC39SsFdeXa+De76z/S3+or0t7HGlUim\nNVkIcWqm/sD2ia8hYberaRRTbUQ1iObNToIg8dA/xna6D61sYK8jkf1GVPpKsCTA\nOVW5u9XrE1f5YQEovE9kFgvtzs0u6jSeI9edqVadH1u6hX4QWQSTrcTb3raqAKpK\nl67cQ96eXI1WQPSdPhQPTjqzOPZDbot3qMkGFijHar7FdQjDx/cNhqhvxv0LWsvl\njgep1czUFoo1BS3wTUiO0qyloNGOQdgmlTOHbMFk1wgoNyAohfZtfn6LH/zlJnE3\nQ0YkUKgAG+1N/PmkQFO0k5qAflUV7h+HAzT1ZAZcscjHNbQFDc0Zjq9nE9sfhxE8\nOFpnF9Jp3fQVekyyC/dsCxtJdYfhxqYe+BzZu0SlsLCmc1JoK5lkiXQwv6+cFpKW\nwfHMTTrCoOetJyiF7oJX+t4adzmLmnujiw5izxObWQJ7avHC1oYNHfRejrOtlu34\n0nDPRFiSDyEbDCBXPe9dIafqjJVLQGFOeXC8/VN9cGSZp2JV8rqumWOr9E+Wd5zU\n8MRZpevo0i3rPgdyFRpw\n-----END ENCRYPTED PRIVATE KEY-----\n-----BEGIN CERTIFICATE-----\nMIIEaDCCA1CgAwIBAgIPANHZYxYlOc+wEAEDDWDpMA0GCSqGSIb3DQEBCwUAMFYx\nCzAJBgNVBAYTAkRFMSMwIQYDVQQKExpTQVAgSW9UIFRydXN0IENvbW11bml0eSBJ\nSTEiMCAGA1UEAxMZU0FQIEludGVybmV0IG9mIFRoaW5ncyBDQTAeFw0yMTExMTIw\nNzMyMjNaFw0yMjExMTIwNzMyMjNaMIG1MQswCQYDVQQGEwJERTEcMBoGA1UEChMT\nU0FQIFRydXN0IENvbW11bml0eTEVMBMGA1UECxMMSW9UIFNlcnZpY2VzMXEwbwYD\nVQQDFGhkZXZpY2VBbHRlcm5hdGVJZDoyMTQ1ZGYwZS0zNDUxLTQ2Y2ItYmYyMy0y\nMzE5MWFmNjZmY2V8Z2F0ZXdheUlkOjJ8dGVuYW50SWQ6MTExNjkwMzQ5MHxpbnN0\nYW5jZUlkOmRrZS1xYTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAJeF\naxjV7Xk1R2dFjadN6WsUkrmcVu44vZRCJEbR7Chkg1xcXT6cgIlokO/V4lTgaD6i\neCMKMFegjXzEJQy0dyIWncozcmt6HJFxpdjVQtdCtDtCWykGscNDgvv5ukykOOKI\nMzWJ4d2cJRlostpNe4FYZoPp6cArSHTl9DvfYqjZ/ykeTa1w157dgVxPxezHrJMl\n+z2XgO37mq6CJLw8J6W8RBHbCADgB8c6qGHgJnBURyxnoHHi/yqdIKC6cOs8NAnc\nyVmnvLDu8RUWu9pWkqFHhMvSqdkUCTYORZ9mUTm/Kmv6ss2NaYT4uUBZTskwnAa9\nFLdj+DV2NG0OQl3NYr8CAwEAAaOB0jCBzzBIBgNVHR8EQTA/MD2gO6A5hjdodHRw\nczovL3Rjcy5teXNhcC5jb20vY3JsL1RydXN0Q29tbXVuaXR5SUkvU0FQSW9UQ0Eu\nY3JsMAwGA1UdEwEB/wQCMAAwJQYDVR0SBB4wHIYaaHR0cDovL3NlcnZpY2Uuc2Fw\nLmNvbS9UQ1MwDgYDVR0PAQH/BAQDAgbAMB0GA1UdDgQWBBSRf8DUjowgQ+6amVIs\njd7zM7VWqjAfBgNVHSMEGDAWgBSVt7P1WN7VtLNYRuDypsl4Tr0tdTANBgkqhkiG\n9w0BAQsFAAOCAQEARzSc9GLpSU3pRJPIfgadHrZ+2KQsPsQ1/fLlASlt4V1Rlxn7\n/tn0gk3sP0X5/TrkO+N0kx1qrLarxWSDiVfaXoPa6Lit30SBPnPLUPPPZeTJOz5r\nTW9PkPPuC39GlM1biVoil2cLZrTr9DMSUoBvR4IVKQoJveQsLwn7Ea+SDPE0uvZV\nbDN6UPGZ2yIiCXO1MODJ6r3A4EDD2MArGgfhGdbvJNAY36ShFJhzfzi0t8linEAA\nxh0vcaEEIkVeEiwiguyGWB69X88cjZ0Q5cCf0r6iu3oQnB57uM5TW12OwXQN1NpQ\neK3EMFSoM6BYJu/3B8TXhNmpNBvD7KYozw9XaA==\n-----END CERTIFICATE-----\n"
+    }
+}
+
+
+
+
 import agrirouter as ar
 from agrirouter.onboarding.enums import GateWays
 from agrirouter.messaging.enums import CapabilityType
@@ -52,7 +78,7 @@ from agrirouter.utils.uuid_util import new_uuid
 application_id = "8c947a45-c57d-42d2-affc-206e21d63a50"		# # store here your application id. You can find it in AR UI
 
 
-def test_auth():
+def example_auth():
     print("Authorization...\n")
 
     auth_params = ar.AuthUrlParameter(application_id=application_id, response_type="onboard")
@@ -83,9 +109,9 @@ def test_auth():
     return auth_data
 
 
-def test_onboarding(gateway_id):
+def example_onboarding(gateway_id):
 
-    auth_data = test_auth()
+    auth_data = example_auth()
 
     print("Onboarding...\n")
 
@@ -108,9 +134,14 @@ def test_onboarding(gateway_id):
     return onboarding_response
 
 
-def test_list_endpoints_mqtt(onboarding_response):
+def example_list_endpoints_mqtt(onboarding_response_data, foo):
+    onboarding_response = SoftwareOnboardingResponse()
+    onboarding_response.json_deserialize(onboarding_response_data)
+
     messaging_service = MqttMessagingService(
-        onboarding_response=onboarding_response
+        onboarding_response=onboarding_response,
+        on_message_callback=foo
+
     )
     list_endpoint_parameters = ListEndpointsParameters(
         technical_message_type=CapabilityType.ISO_11783_TASKDATA_ZIP.value,
@@ -121,11 +152,19 @@ def test_list_endpoints_mqtt(onboarding_response):
         application_message_seq_no=1,
     )
     list_endpoint_service = ListEndpointsService(messaging_service)
+
     messaging_result = list_endpoint_service.send(list_endpoint_parameters)
-    return messaging_result
+    print("Sent message: ", messaging_result)
+
+    # Is needed for waiting of messaging responses from outbox
+    while True:
+        time.sleep(1)
 
 
-def test_list_endpoint_http(onboarding_response):
+def example_list_endpoints_http(onboarding_response_data):
+    onboarding_response = SoftwareOnboardingResponse()
+    onboarding_response.json_deserialize(onboarding_response_data)
+
     messaging_service = HttpMessagingService()
     list_endpoint_parameters = ListEndpointsParameters(
         technical_message_type=CapabilityType.ISO_11783_TASKDATA_ZIP.value,
@@ -136,14 +175,19 @@ def test_list_endpoint_http(onboarding_response):
         application_message_seq_no=1,
     )
     list_endpoint_service = ListEndpointsService(messaging_service)
+
     messaging_result = list_endpoint_service.send(list_endpoint_parameters)
+    print("Sent message: ", messaging_result)
+
     return messaging_result
 
 
-def test_subscription_http(onboarding_response):
+def example_subscription_http(onboarding_response_data):
+    onboarding_response = SoftwareOnboardingResponse()
+    onboarding_response.json_deserialize(onboarding_response_data)
+
     messaging_service = HttpMessagingService()
     subscription_service = SubscriptionService(messaging_service)
-
     tmt = CapabilityType.ISO_11783_TASKDATA_ZIP.value
     subscription_item = Subscription.MessageTypeSubscriptionItem(technical_message_type=tmt)
     subscription_parameters = SubscriptionParameters(
@@ -152,9 +196,63 @@ def test_subscription_http(onboarding_response):
         application_message_id=new_uuid(),
         application_message_seq_no=1,
     )
+
     messaging_result = subscription_service.send(subscription_parameters)
+    print("Sent message: ", messaging_result)
+
     return messaging_result
 
 
+def example_subscription_mqtt(onboarding_response_data, on_msg_callback):
+    onboarding_response = SoftwareOnboardingResponse()
+    onboarding_response.json_deserialize(onboarding_response_data)
+
+    messaging_service = MqttMessagingService(onboarding_response, on_message_callback=on_msg_callback)
+    subscription_service = SubscriptionService(messaging_service)
+    tmt = CapabilityType.ISO_11783_TASKDATA_ZIP.value
+    subscription_item = Subscription.MessageTypeSubscriptionItem(technical_message_type=tmt)
+    subscription_parameters = SubscriptionParameters(
+        subscription_items=[subscription_item],
+        onboarding_response=onboarding_response,
+        application_message_id=new_uuid(),
+        application_message_seq_no=1,
+    )
+
+    messaging_result = subscription_service.send(subscription_parameters)
+    print("Sent message: ", messaging_result)
+
+    # Is needed for waiting of messaging responses from outbox
+    while True:
+        time.sleep(1)
+
+
+def on_message_callback(client, userdata, msg):
+
+    # Define here the way receiving messages will be processed
+
+    from agrirouter.messaging.decode import decode_response
+    from agrirouter.messaging.decode import decode_details
+    from agrirouter.messaging.messages import OutboxMessage
+
+    outbox_message = OutboxMessage()
+    outbox_message.json_deserialize(msg.payload.decode().replace("'", '"'))
+
+    print(outbox_message.command.message)
+
+    decoded_message = decode_response(outbox_message.command.message)
+    print(decoded_message.response_envelope)
+
+    try:
+        decoded_details = decode_details(decoded_message.response_payload.details)
+        print(decoded_details)
+    except:
+        pass
+
+
 if __name__ == "__main__":
-    test_list_endpoints_mqtt(test_onboarding(GateWays.MQTT.value))
+    onboarding_response_mqtt = example_onboarding(GateWays.MQTT.value)
+    example_list_endpoints_mqtt(onboarding_response_mqtt.json_serialize(), on_message_callback)
+
+    # of for http
+    onboarding_response_mqtt = example_onboarding(GateWays.REST.value)
+    example_list_endpoints_http(onboarding_response_mqtt.json_serialize())

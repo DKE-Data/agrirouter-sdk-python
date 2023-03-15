61d55169503a8152761ad7c7d8dcc8912dfbc0c9
YashRunwal <yashrunwal18@gmail.com>
2023-03-15 10:05:31 +0100

    Changed made to the code according to the comments. Identifer class added , test script changed

--- tests/data/identifier.py
+++ tests/data/identifier.py
@@ -1,4 +1,20 @@
-RECIPIENT = "Http/CommunicationUnit/Recipient"
-SENDER = "Http/CommunicationUnit/Sender"
-MQTT_RECIPIENT = "Mqtt/CommunicationUnit/Recipient"
-MQTT_SENDER = "Mqtt/CommunicationUnit/Sender"
+class Identifier:
+    HTTP_RECIPIENT = {
+        'Path': "Http/CommunicationUnit/Recipient",
+        'id': "646a1f40-c275-11ed-afa1-0242ac120002"
+    }
+
+    HTTP_SENDER = {
+        'Path': "Http/CommunicationUnit/Recipient",
+        'id': "6bd7796c-c275-11ed-afa1-0242ac120002"
+    }
+
+    MQTT_RECIPIENT = {
+        'Path': "Mqtt/CommunicationUnit/Recipient",
+        'id': "dc1b27ce-c283-11ed-afa1-0242ac120002"
+    }
+
+    MQTT_SENDER = {
+        'Path': "Mqtt/CommunicationUnit/Sender",
+        'id': "e6bf88f0-c283-11ed-afa1-0242ac120002"
+    }
--- tests/onboarding_test/test_onboarding_process_mqtt.py
+++ tests/onboarding_test/test_onboarding_process_mqtt.py
@@ -5,13 +5,12 @@ from agrirouter.environments.environments import QAEnvironment
 from agrirouter.onboarding.enums import CertificateTypes, GateWays
 from agrirouter.onboarding.parameters import OnboardParameters
 from agrirouter.onboarding.response import OnboardResponse
-from tests.data import identifier
 from tests.data.applications import CommunicationUnit
 from tests.data.onboard_response_integration_service import OnboardResponseIntegrationService
-from tests.constants import cu_recipient_endpoint_id
+from tests.data.identifier import Identifier
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def onboarding_process_fixture():
     def __onboard(uuid: str, _environment, registration_code: str, certification_type_definition: str = "PEM",
                   gateway_id: str = "2") -> OnboardResponse:
@@ -44,30 +43,62 @@ def onboarding_process_fixture():
 class TestSingleMqttEndpointWithPEMCertificate:
     _environment = QAEnvironment()
 
+    @pytest.mark.skip(reason="Will fail unless registration code is changed")
     def test_update_recipient_with_PEM(self, onboarding_process_fixture):
         onboard_response = onboarding_process_fixture(
-            uuid=cu_recipient_endpoint_id,
+            uuid=Identifier.MQTT_RECIPIENT['id'],
             _environment=self._environment,
-            registration_code="032ecf9480",
+            registration_code="78ddb81011",
             certification_type_definition=str(CertificateTypes.PEM.value),
             gateway_id=str(GateWays.MQTT.value)
         )
 
-        # self._enable_all_capabilities_via_mqtt(onboard_response)
-        OnboardResponseIntegrationService.save(identifier.MQTT_RECIPIENT, onboard_response)
+        OnboardResponseIntegrationService.save(Identifier.MQTT_RECIPIENT['Path'], onboard_response)
 
 
 class TestSingleMqttEndpointWithP12Certificate:
     _environment = QAEnvironment()
 
+    @pytest.mark.skip(reason="Will fail unless registration code is changed")
     def test_update_recipient_with_P12(self, onboarding_process_fixture):
         onboard_response = onboarding_process_fixture(
-            uuid=cu_recipient_endpoint_id,
+            uuid=Identifier.MQTT_RECIPIENT['id'],
             _environment=self._environment,
-            registration_code="74cb298919",
+            registration_code="27e788781b",
             certification_type_definition=str(CertificateTypes.P12.value),
             gateway_id=str(GateWays.MQTT.value)
         )
 
-        # self._enable_all_capabilities_via_mqtt(onboard_response)
-        OnboardResponseIntegrationService.save(identifier.MQTT_RECIPIENT, onboard_response)
+        OnboardResponseIntegrationService.save(Identifier.MQTT_RECIPIENT['Path'], onboard_response)
+
+
+class TestSenderSingleMqttEndpointWithPEMCertificate:
+    _environment = QAEnvironment()
+
+    @pytest.mark.skip(reason="Will fail unless registration code is changed")
+    def test_update_sender_with_PEM(self, onboarding_process_fixture):
+        onboard_response = onboarding_process_fixture(
+            uuid=Identifier.MQTT_SENDER['id'],
+            _environment=self._environment,
+            registration_code="8fb74f7c85",
+            certification_type_definition=str(CertificateTypes.PEM.value),
+            gateway_id=str(GateWays.MQTT.value)
+        )
+
+        OnboardResponseIntegrationService.save(Identifier.MQTT_SENDER['Path'], onboard_response)
+
+
+class TestSenderSingleMqttEndpointWithP12Certificate:
+    _environment = QAEnvironment()
+
+    @pytest.mark.skip(reason="Will fail unless registration code is changed")
+    def test_update_sender_with_P12(self, onboarding_process_fixture):
+        onboard_response = onboarding_process_fixture(
+            uuid=Identifier.MQTT_SENDER['id'],
+            _environment=self._environment,
+            registration_code="1c14568c64",
+            certification_type_definition=str(CertificateTypes.P12.value),
+            gateway_id=str(GateWays.MQTT.value)
+        )
+
+        OnboardResponseIntegrationService.save(Identifier.MQTT_SENDER['Path'], onboard_response)

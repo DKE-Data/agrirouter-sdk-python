e2da25dafed97624704945526dd3d0464a685017
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-03-23 09:25:32 +0100

    Adapt comments.

--- tests/data/identifier.py
+++ tests/data/identifier.py
@@ -1,42 +1,58 @@
 class Identifier:
-    # HTTP Protocol
+    """ Identifier for the endpoints, used for the test cases. """
+
     HTTP_RECIPIENT_P12 = {
+        """ Identifier for the HTTP recipient endpoint with a P12 certificate. """
+
         'Path': "Http/CommunicationUnit/P12/Recipient",
         'id': "646a1f40-c275-11ed-afa1-0242ac120002"
     }
 
     HTTP_SENDER_P12 = {
+        """ Identifier for the HTTP sender endpoint with a P12 certificate. """
+
         'Path': "Http/CommunicationUnit/P12/Sender",
         'id': "6bd7796c-c275-11ed-afa1-0242ac120002"
     }
 
     HTTP_RECIPIENT_PEM = {
+        """ Identifier for the HTTP recipient endpoint with a PEM certificate. """
+
         'Path': "Http/CommunicationUnit/PEM/Recipient",
         'id': "e5b9de9a-c889-11ed-afa1-0242ac120002"
     }
 
     HTTP_SENDER_PEM = {
+        """ Identifier for the HTTP sender endpoint with a PEM certificate. """
+
         'Path': "Http/CommunicationUnit/PEM/Sender",
         'id': "e9b0d99a-c889-11ed-afa1-0242ac120002"
     }
 
-    # MQTT Protocol
     MQTT_RECIPIENT_P12 = {
+        """ Identifier for the MQTT recipient endpoint with a P12 certificate. """
+
         'Path': "Mqtt/CommunicationUnit/P12/Recipient",
         'id': "dc1b27ce-c283-11ed-afa1-0242ac120002"
     }
 
     MQTT_SENDER_P12 = {
+        """ Identifier for the MQTT sender endpoint with a P12 certificate. """
+
         'Path': "Mqtt/CommunicationUnit/P12/Sender",
         'id': "e6bf88f0-c283-11ed-afa1-0242ac120002"
     }
 
     MQTT_RECIPIENT_PEM = {
+        """ Identifier for the MQTT recipient endpoint with a PEM certificate. """
+
         'Path': "Mqtt/CommunicationUnit/PEM/Recipient",
         'id': "d9786fbc-c888-11ed-afa1-0242ac120002"
     }
 
     MQTT_SENDER_PEM = {
+        """ Identifier for the MQTT sender endpoint with a PEM certificate. """
+
         'Path': "Mqtt/CommunicationUnit/PEM/Sender",
         'id': "083d2374-c889-11ed-afa1-0242ac120002"
     }

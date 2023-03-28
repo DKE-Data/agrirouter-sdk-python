cc6bfdb1ac3d2fef528e447c562dfe2d8107f6c4
YashRunwal <yashrunwal18@gmail.com>
2023-03-28 15:35:07 +0200

    updated the onboard response
    The previously created onboard response is used

--- tests/service/messaging/mqtt/test_capability_service.py
+++ tests/service/messaging/mqtt/test_capability_service.py
@@ -1,48 +1,33 @@
 import pytest
 import time
-from agrirouter.environments.environments import QAEnvironment
 from agrirouter.generated.messaging.request.payload.endpoint.capabilities_pb2 import CapabilitySpecification
 from agrirouter.messaging.decode import decode_details
 from agrirouter.messaging.decode import decode_response
 from agrirouter.messaging.enums import CapabilityType
-from agrirouter.onboarding.enums import GateWays, CertificateTypes
 from agrirouter.messaging.messages import OutboxMessage
 from agrirouter.messaging.parameters.service import CapabilitiesParameters
 from agrirouter.messaging.services.commons import MqttMessagingService
 from agrirouter.messaging.services.messaging import CapabilitiesService
-from agrirouter.onboarding.onboarding import OnboardingService
-from agrirouter.onboarding.parameters import OnboardParameters
-from agrirouter.onboarding.response import OnboardResponse
 from tests.data.applications import CommunicationUnit
+from tests.data.onboard_response_integration_service import OnboardResponseIntegrationService
 from tests.data.identifier import Identifier
 import logging
 
 
 class TestMqttCapabilitiesService:
 
-    @pytest.mark.skip(reason="Will fail unless the registration code is changed")
+    # @pytest.mark.skip(reason="Will fail unless the registration code is changed")
     def test_update_recipient(self):
-        self._onboard_response = self._onboard(uuid=Identifier.MQTT_RECIPIENT_PEM[Identifier.ID],
-                                               registration_code="49667e828f",
-                                               certification_type_definition=CertificateTypes.PEM.value,
-                                               gateway_id=GateWays.MQTT.value)
+        """
+            Load Onboard Response from 'Mqtt/CommunicationUnit/PEM/Recipient'
+        """
+        self._onboard_response = TestMqttCapabilitiesService.load_onboard_response(r'Mqtt/CommunicationUnit/PEM/Recipient')
         self._response = self._enable_all_capabilities_via_mqtt(self._onboard_response, self._on_message_callback)
 
-    def _onboard(self, uuid: str, registration_code: str, certification_type_definition: str = "P12",
-                 gateway_id: str = "2") -> OnboardResponse:
-        onboarding_service = OnboardingService(env=QAEnvironment())
-        onboarding_parameters = OnboardParameters(
-            id_=uuid,
-            reg_code=registration_code,
-            certificate_type=certification_type_definition,
-            gateway_id=gateway_id,
-            application_id=CommunicationUnit.application_id,
-            certification_version_id=CommunicationUnit.certification_version_id,
-            time_zone="+01:00"
-        )
-
-        onboard_response = onboarding_service.onboard(onboarding_parameters)
-        return onboard_response
+    @staticmethod
+    def load_onboard_response(path):
+        recorded_onboard_response = OnboardResponseIntegrationService.read(path)
+        return recorded_onboard_response
 
     def _on_message_callback(self, client, userdata, msg):
         outbox_message = OutboxMessage()

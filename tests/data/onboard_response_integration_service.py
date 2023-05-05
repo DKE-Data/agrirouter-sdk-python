269d004263c83e49021dca44cb19adecbff98202
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-05-05 08:01:45 +0200

    Add missing comments.

--- tests/data/onboard_response_integration_service.py
+++ tests/data/onboard_response_integration_service.py
@@ -3,9 +3,13 @@ import sys, os
 import tests
 import json
 
+
 class OnboardResponseIntegrationService:
     @staticmethod
     def read(identifier: str) -> OnboardResponse:
+        """
+        Read a recorded onboard response from the filesystem.
+        """
         path = OnboardResponseIntegrationService._get_path(identifier)
         with open(path) as f:
             contents = f.read()
@@ -15,6 +19,9 @@ class OnboardResponseIntegrationService:
 
     @staticmethod
     def save(identifier: str, onboard_response: OnboardResponse):
+        """
+        Save a recorded onboard response to the filesystem.
+        """
         path = OnboardResponseIntegrationService._get_path(identifier)
         os.makedirs(os.path.dirname(path), exist_ok=True)
         data = json.dumps(onboard_response.json_serialize())
@@ -23,6 +30,9 @@ class OnboardResponseIntegrationService:
 
     @staticmethod
     def _get_path(identifier: str) -> str:
+        """
+        Get the path to a recorded onboard response.
+        """
         path = os.path.join(
             os.path.dirname(tests.__file__),
             f"data/onboarding_responses/{identifier}.json"

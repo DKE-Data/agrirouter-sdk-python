10a6c5c735ad98b733c7b37c79bf28e748fba8d5
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-08-22 15:31:28 +0200

    Removed redundant test files and fixed newline error
    Deleted unnecessary test files, 'test_onboard_response_integration_service.py' and 'test_update_onboard_responses.py', to improve the simplicity and readability of the project. Also fixed the absence of a newline issue at the end of file in 'identifier.py' to adhere to code style guidelines.

--- tests/internal/helper_test/test_onboard_response_integration_service.py
+++
@@ -1,5 +0,0 @@
-from tests.data.onboard_response_integration_service import OnboardResponseIntegrationService
-
-
-def test_onboard_response_integration_service():
-    onboard_response = OnboardResponseIntegrationService.read("test")

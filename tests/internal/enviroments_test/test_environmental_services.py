8848aff84e2ac7dd62fd8954b96348f925dde2c0
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-03-12 14:01:24 +0100

    Refactor import paths and use enum for environments
    This commit refactors the import paths for environment-related modules, organizing them under the 'api' directory for better structure. It also replaces instances of strings used to denote an environment with the corresponding enumerated types, enhancing code clarity and preventing potential errors.

--- tests/internal/enviroments_test/test_environmental_services.py
+++ tests/internal/enviroments_test/test_environmental_services.py
@@ -2,13 +2,14 @@
 
 import pytest
 
-from agrirouter.environments.environmental_services import EnvironmentalService
+from agrirouter.api.enums import Environments
 from agrirouter.api.exceptions import InvalidEnvironmentSetup
+from agrirouter.environments.environmental_services import EnvironmentalService
 from tests.common.constants import ENV
 
 
 def test_arclient_set_env():
     assert EnvironmentalService(ENV)._set_env(ENV) is None
-    assert EnvironmentalService("Production")._set_env("Production") is None
+    assert EnvironmentalService(env=Environments.PRODUCTION.value)._set_env(Environments.PRODUCTION.value) is None
     with pytest.raises(InvalidEnvironmentSetup):
         assert EnvironmentalService("WRONG")._set_env("WRONG")

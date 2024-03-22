ef7dcc5ed7fe772c4729b5c7dce5440971a9ba85
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-03-22 11:27:57 +0100

    Update CI/CD workflows and project metadata
    Updated the Python versions under test in the CI/CD workflows to include Python 3.12. Also, made changes in the pyproject.toml and setup.py files, reflecting the updated metadata, including authors, readme, classifiers, etc. The setuptools calls in setup.py were significantly simplified.

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    packages=find_packages(),
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-    project_urls={
-        'Documentation': 'https://github.com/DKE-Data/agrirouter-sdk-python',
-        'Source': 'https://github.com/DKE-Data/agrirouter-sdk-python',
-    },
-)
+setup()

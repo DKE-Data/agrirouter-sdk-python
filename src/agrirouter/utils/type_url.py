d9af01852353d578becd1ea905f1719493fd8e99
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-04-11 14:08:17 +0200

    Update imports in type_url.py
    The import order and placement in type_url.py has been corrected to improve code readability and organization. Along with this, import statements for TimeLog and ISO11783_TaskData from efdi_pb2 and TypeUrlNotFound from exceptions have been moved to the top.

--- src/agrirouter/utils/type_url.py
+++ src/agrirouter/utils/type_url.py
@@ -1,7 +1,10 @@
-from src.agrirouter.generated.cloud_provider_integration.cloud_virtualized_app_registration_pb2 import OnboardingResponse, \
+from src.agrirouter.api.exceptions import TypeUrlNotFound
+from src.agrirouter.generated.cloud_provider_integration.cloud_virtualized_app_registration_pb2 import \
+    OnboardingResponse, \
     OnboardingRequest
 from src.agrirouter.generated.commons.message_pb2 import Messages
 from src.agrirouter.generated.messaging.request.payload.account.endpoints_pb2 import ListEndpointsQuery
+from src.agrirouter.generated.messaging.request.payload.efdi.efdi_pb2 import TimeLog, ISO11783_TaskData
 from src.agrirouter.generated.messaging.request.payload.endpoint.capabilities_pb2 import CapabilitySpecification
 from src.agrirouter.generated.messaging.request.payload.endpoint.subscription_pb2 import Subscription
 from src.agrirouter.generated.messaging.request.payload.feed.feed_requests_pb2 import MessageDelete, MessageConfirm, \
@@ -9,11 +12,7 @@ from src.agrirouter.generated.messaging.request.payload.feed.feed_requests_pb2 i
 from src.agrirouter.generated.messaging.response.payload.account.endpoints_pb2 import ListEndpointsResponse
 from src.agrirouter.generated.messaging.response.payload.feed.feed_response_pb2 import HeaderQueryResponse, \
     MessageQueryResponse
-
-from src.agrirouter.generated.messaging.request.payload.efdi.efdi_pb2 import TimeLog, ISO11783_TaskData
-
 from src.agrirouter.generated.messaging.response.payload.feed.push_notification_pb2 import PushNotification
-from src.agrirouter.api.exceptions import TypeUrlNotFound
 
 
 class TypeUrl:
